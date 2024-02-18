edd7e1dd41074071cf9ff568e27d3011cddd9ebc
MRAkB777 <160231017+MRAkB777@users.noreply.github.com>
2024-02-18 08:10:01 +0000

    conversion

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,18 +6,47 @@
 // let name = "kuldeep"
 
 
-let score = null
-// console.log(typeof score );
-// console.log(typeof (score));
+// let score = null
+// // console.log(typeof score );
+// // console.log(typeof (score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33
-let 
+// let someNumber = 33
+// let 
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " kuldeep"
+
+// let str3 = str1 + str2 
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + '2');
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter  = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+/// Assignment Study Prefix and postfix from MDN
+
