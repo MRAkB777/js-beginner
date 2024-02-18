5116370404c473903b95b9fc289831a096b8f286
MRAkB777 <160231017+MRAkB777@users.noreply.github.com>
2024-02-18 07:13:34 +0000

    conversion of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "kuldeeepb162005@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// accountId = 2 //
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// "use strict"; //treat all JS code as newer version
+
+// // alert( 3 + 3) // we are using node JS not browser
+
+
+// let name = "kuldeep"
+
+
+let score = null
+// console.log(typeof score );
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let 
