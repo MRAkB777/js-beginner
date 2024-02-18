cfae68362473b3a57d6c87f7d980af2e05290cf4
MRAkB777 <160231017+MRAkB777@users.noreply.github.com>
2024-02-18 12:01:54 +0000

    comparison

---
+++ 01_basics/01_comparison.js
@@ -0,0 +1,12 @@
+console.log("2" > 1 );
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
+
+//  **************      ********************//
+
+// Data Types:::::--- Stack(Primitive), Heap (Non-primitive)
+
+const name = "kuldeep"
