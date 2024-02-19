5745dd8d4dde9a2862ca88a88a38ec8b0560a544
MRAkB777 <160231017+MRAkB777@users.noreply.github.com>
2024-02-19 11:22:30 +0000

    strings

---
+++ 01_basics/01_strings.js
@@ -0,0 +1,7 @@
+const name = "Kuldeep"
+const repoCount = 50
+
+console.log(name + repoCount + " value");
+console.log(`Hello my name is ${name } and my repo count is ${repoCount}`);
+
+const newString = gameName.substring(0, 4)
