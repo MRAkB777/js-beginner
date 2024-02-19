e133643de68d007178ffda2e538f93e1fb93a760
MRAkB777 <160231017+MRAkB777@users.noreply.github.com>
2024-02-19 15:04:48 +0000

    dates

--- 01_basics/datesin.js
+++ 01_basics/datesin.js
@@ -1,6 +1,17 @@
 // Dates
 
 let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toISOString());
-console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 0, 19)     // Month starts from 0 in JS
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+newDate.toLocaleString('default', {
+    weekday
+})
