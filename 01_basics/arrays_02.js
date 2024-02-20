03f39bc1cb35d60500f904397da87d17095a7621
MRAkB777 <160231017+MRAkB777@users.noreply.github.com>
2024-02-20 11:36:30 +0000

    arraysp2

---
+++ 01_basics/arrays_02.js
@@ -0,0 +1,10 @@
+const marvel_heroes = ["Ironman", "Thor", "CaptainAmerica"]
+const dc_heroes = ["Batman", "Superman", "Flash"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+
+marvel_heroes.concat(dc_heroes)
+
+console.log(marvel_heroes);
