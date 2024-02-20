d14b0f3ffbec0e5d0f1a0247c91ceca89c1cddf8
MRAkB777 <160231017+MRAkB777@users.noreply.github.com>
2024-02-20 12:38:37 +0000

    arraysp2

--- 01_basics/arrays_02.js
+++ 01_basics/arrays_02.js
@@ -5,6 +5,23 @@ const dc_heroes = ["Batman", "Superman", "Flash"]
 
 // console.log(marvel_heroes);
 
-marvel_heroes.concat(dc_heroes)
+const allHeroes = marvel_heroes.concat(dc_heroes)
 
-console.log(marvel_heroes);
+// console.log(allHeroes);
+
+
+const all_new_heros = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6, [7, 8, 9, [1, 12, 22, 23, 12]]]]
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("Kuldeep"));
+console.log(Array.from("Kuldeep"));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
